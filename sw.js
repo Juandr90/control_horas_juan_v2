3c91030874d1636d88fa94a5817404f80863808e
Juandr90 <89923177+Juandr90@users.noreply.github.com>
2025-10-18 10:04:28 +0200

    Update cache files in service worker
    Removed 'xlsx.full.min.js' from the cache list.

--- sw.js
+++ sw.js
@@ -1,5 +1,4 @@
-
 const CACHE_NAME = 'control-horas-juan-v2-v1';
-const FILES_TO_CACHE = ['/', '/index.html', '/styles.css', '/app_v2.js', '/manifest.json', '/icon.png', '/xlsx.full.min.js'];
+const FILES_TO_CACHE = ['/', '/index.html', '/styles.css', '/app_v2.js', '/manifest.json', '/icon.png'];
 self.addEventListener('install', evt=>{ evt.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(FILES_TO_CACHE))); });
 self.addEventListener('fetch', evt=>{ evt.respondWith(caches.match(evt.request).then(resp=>resp||fetch(evt.request))); });
